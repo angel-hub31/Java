89a4e45fc34571204fdb9cbac4129cc07c815527
angel-hub31 <moralesmales1@hotmail.com>
2026-03-09 21:29:58 -0500

    Reto 9 METODOS

---
+++ Clases/src/com/krakedev/Mensajeria.java
@@ -0,0 +1,22 @@
+package com.krakedev;
+
+public class Mensajeria {
+	// "void" no retorna nada y no recibe nada
+	public void saludar() {
+		System.out.println("Hola Mundo");
+		
+	}
+	
+	public void saludarAmigo(String nombre,String apellido) {
+		System.out.println("Hola " +nombre+ " " +apellido);
+	}
+	
+	///funcion mostrar resultado
+	
+	public void mostrarResultado() {
+		
+		System.out.println("Ahorita no joven, Sali al almuerzo. Regreso en 15 minutos");
+
+	}
+
+}
---
+++ Clases/src/com/krakedev/TestCalculadora.java
@@ -0,0 +1,37 @@
+package com.krakedev;
+
+public class TestCalculadora {
+
+	public static void main(String[] args) {
+		Calculadora calcu;//creamos una variable calcu
+		calcu=new Calculadora();
+		
+		int resultadoSuma;
+		resultadoSuma=calcu.sumar(5, 8);
+		System.out.println("Resultado:"+ resultadoSuma);
+		
+		int resultadoResta;
+		resultadoResta=calcu.restar(10, 5);
+		System.out.println("Resultado:"+ resultadoResta);
+		
+		double resultadoMultiplicacion;
+		resultadoMultiplicacion=calcu.multiplicar(10, 5);
+		System.out.println("Resultado:"+ resultadoMultiplicacion);
+		
+		double resultadoDividir;
+		resultadoDividir=calcu.dividir(10,2);
+		System.out.println("Resultado:"+resultadoDividir);
+		
+		double resultadoPromedio;
+		resultadoPromedio=calcu.promediar(10,8,9);
+		System.out.println("Resultado:"+resultadoPromedio);
+		
+		calcu.mostrarResultado();
+		
+
+	}
+	
+	
+	
+
+}
