146f6f533c7b84e4fa43adaae2644a00c6bf067f
angel-hub31 <moralesmales1@hotmail.com>
2026-03-11 20:24:39 -0500

    encapsulamiento de la clase Auto

--- Clases/src/com/krakedev/TestAuto.java
+++ Clases/src/com/krakedev/TestAuto.java
@@ -4,24 +4,24 @@ public class TestAuto {
 
 	public static void main(String[] args) {
 		Auto auto1=new Auto();
-		auto1.marca="nissan";
-		auto1.anio=2025;
-		auto1.precio=25000;
+		auto1.setMarca("nissan");
+		auto1.setAnio(2025);
+		auto1.setPrecio(25000);
 		
-		System.out.println("Marca:" + auto1. marca );
-		System.out.println("Anio:" + auto1. anio );
-		System.out.println("Precio:" + auto1. precio );
+		System.out.println("Marca:" + auto1.getMarca() );
+		System.out.println("Anio:" + auto1.getAnio() );
+		System.out.println("Precio:" + auto1.getPrecio ());
 		
 		System.out.println("***************************************" );
 		
 		Auto auto2=new Auto();
-		auto2.marca="ford";
-		auto2.anio=2026;
-		auto2.precio=50000;
+		auto2.setMarca("ford");
+		auto2.setAnio(2026);
+		auto2.setPrecio(50000);
 		
-		System.out.println("Marca:" + auto2. marca );
-		System.out.println("Anio:" + auto2. anio );
-		System.out.println("Precio:" + auto2. precio );
+		System.out.println("Marca:" + auto2.getMarca ());
+		System.out.println("Anio:" + auto2.getAnio() );
+		System.out.println("Precio:" + auto2.getPrecio() );
 	
 
 	}
