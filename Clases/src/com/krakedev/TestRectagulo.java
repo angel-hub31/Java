dfe44254620bf3036944504e53ff429e93f1290e
angel-hub31 <moralesmales1@hotmail.com>
2026-03-11 21:19:30 -0500

    encapsulamiento  de la clase Rectangulo

--- Clases/src/com/krakedev/TestRectagulo.java
+++ Clases/src/com/krakedev/TestRectagulo.java
@@ -8,20 +8,20 @@ public class TestRectagulo {
 		Rectangulo p=new Rectangulo();
 		
 		int areaR1;
-		r1.base=10;
-		r1.altura=5;
+		r1.setBase(10);
+		r1.setAltura(5);
 		areaR1=r1.calcularArea();
 		System.out.println("Area de r1:"+ areaR1);
 				
 		int areaR2;
-		r2.base=8;
-		r2.altura=3;
+		r2.setBase(8);
+		r2.setAltura(3);
 		areaR2=r2.calcularArea();
 		System.out.println("Area de r2:"+ areaR2);
 		
 		int  perimetroP;
-		p.base=4;
-		p.altura=2;
+		p.setBase(4);
+		p.setAltura(2);
 		perimetroP=p.calcularPerimetro();
 		System.out.println("Perimetro de p:"+ perimetroP);
 
