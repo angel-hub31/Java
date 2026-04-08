1c4e1a63d5e8816d4dcd650d989ebd7d892b0163
angel-hub31 <moralesmales1@hotmail.com>
2026-04-07 22:51:35 -0500

    se crea la clase TestVender

---
+++ EvaluacionAMM/src/com/clearminds/test/TestVender.java
@@ -0,0 +1,24 @@
+package com.clearminds.test;
+
+import com.clearminds.componentes.MaquinaDulces;
+import com.clearminds.componentes.Producto;
+
+public class TestVender {
+
+	public static void main(String[] args) {
+		MaquinaDulces maquina=new MaquinaDulces();
+		maquina.configurarMaquina("A1", "A2", "B1", "B2");
+		
+		Producto producto=new Producto("Papitas",0.85,"KE34");
+		maquina.cargarProducto(producto, "B1", 4);
+		
+		Producto producto2=new Producto("Doritos",0.70,"D456");
+		maquina.cargarProducto(producto2, "A1", 6);
+		
+		maquina.vender("A1");
+		maquina.vender("B1");
+		
+		maquina.mostrarProducto();
+	}
+
+}
